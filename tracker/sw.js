806802fb8ed59db67a32f115f64fa0c3fcf35247
garciaj25 <ca.jairogarcia@gmail.com>
2026-07-23 22:48:10 -0700

    Tracker Rev 1

--- tracker/sw.js
+++ tracker/sw.js
@@ -1,6 +1,6 @@
 /* Sprinkler Tracker service worker.
    Bump CACHE when you upload a new index.html, or devices keep serving the old page. */
-const CACHE = 'sprinkler-tracker-v2';
+const CACHE = 'sprinkler-tracker-v3';
 
 const SHELL = [
   './',
